2ac0b63dfc0c663455a36d7f31e9deb521ea3edd
Fohlen <lennard@berger.work>
2015-04-01 21:31:30 +0200

    Added a base template :)

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 // Kickstart the framework
 $f3=require('lib/base.php');
 
-$f3->set('DEBUG',1);
 if ((float)PCRE_VERSION<7.9)
 	trigger_error('PCRE version is out of date');
 
@@ -12,7 +11,7 @@ $f3->config('config.ini');
 
 $f3->route('GET /',
 	function($f3) {
-		$classes=array(
+		/*$classes=array(
 			'Base'=>
 				array(
 					'hash',
@@ -66,15 +65,8 @@ $f3->route('GET /',
 			'Web\Pingback'=>
 				array('dom','xmlrpc')
 		);
-		$f3->set('classes',$classes);
+		$f3->set('classes',$classes);*/
 		$f3->set('content','welcome.htm');
-		echo View::instance()->render('layout.htm');
-	}
-);
-
-$f3->route('GET /userref',
-	function($f3) {
-		$f3->set('content','userref.htm');
 		echo View::instance()->render('layout.htm');
 	}
 );
