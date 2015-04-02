716901885e3105b5ff645e5407dc87ec1fa96de5
Fohlen <lennard@berger.work>
2015-04-02 12:37:00 +0200

    Added a global database object & configuration.

--- index.php
+++ index.php
@@ -12,63 +12,19 @@ if ((float)PCRE_VERSION<7.9)
 // Load configuration
 $f3->config('config.ini');
 
+// Prepare the database
+$dbConfig = $f3->get('database');
+if (!isset($dbConfig['port'])) 
+	$dbConfig['port'] = 3306;
+
+$f3->set('DB', new DB\SQL(
+	'mysql:host='.$dbConfig['host'].';port='.$dbConfig['port'].';dbname='.$dbConfig['database'],
+	$dbConfig['user'],
+	$dbConfig['password']
+));
+
 $f3->route('GET /',
 	function($f3) {
-		/*$classes=array(
-			'Base'=>
-				array(
-					'hash',
-					'json',
-					'session'
-				),
-			'Cache'=>
-				array(
-					'apc',
-					'memcache',
-					'wincache',
-					'xcache'
-				),
-			'DB\SQL'=>
-				array(
-					'pdo',
-					'pdo_dblib',
-					'pdo_mssql',
-					'pdo_mysql',
-					'pdo_odbc',
-					'pdo_pgsql',
-					'pdo_sqlite',
-					'pdo_sqlsrv'
-				),
-			'DB\Jig'=>
-				array('json'),
-			'DB\Mongo'=>
-				array(
-					'json',
-					'mongo'
-				),
-			'Auth'=>
-				array('ldap','pdo'),
-			'Bcrypt'=>
-				array(
-					'mcrypt',
-					'openssl'
-				),
-			'Image'=>
-				array('gd'),
-			'Lexicon'=>
-				array('iconv'),
-			'SMTP'=>
-				array('openssl'),
-			'Web'=>
-				array('curl','openssl','simplexml'),
-			'Web\Geo'=>
-				array('geoip','json'),
-			'Web\OpenID'=>
-				array('json','simplexml'),
-			'Web\Pingback'=>
-				array('dom','xmlrpc')
-		);*/
-		//$f3->set('classes',$classes);
 		$f3->set('content','welcome.htm');
 		echo View::instance()->render('layout.htm');
 	}
